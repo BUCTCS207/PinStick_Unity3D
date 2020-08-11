58c213dcface89f1483ab1d5cddde44dff3e716d
BUCTCS207 <67367452+BUCTCS207@users.noreply.github.com>
2020-08-11 18:32:10 +0800

    Unity3D_Pinstick

---
+++ StickPinProject/Assets/Scripts/Rotaself.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotaself : MonoBehaviour
+{
+
+    public float speed = 90;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, -speed * Time.deltaTime));
+    }
+}
